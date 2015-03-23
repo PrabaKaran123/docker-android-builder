0ce3a578cf7063c7abe588f062051558b5647f1f
Ilkka Tengvall <ilkka.tengvall@iki.fi>
2015-03-23 21:59:21 +0200

    protect config path for special characters

--- run.sh
+++ run.sh
@@ -29,7 +29,7 @@ grep -q "/"$BUILD_NAME"_ccache" .gitignore || echo "/"$BUILD_NAME"_ccache" >> .g
 grep -q "$BUILD_NAME" .dockerignore  || echo "$BUILD_NAME" >> .dockerignore 
 grep -q $BUILD_NAME"_ccache" .dockerignore || echo $BUILD_NAME"_ccache" >> .dockerignore 
 if [ "$MY_DIR" != "$(pwd)" ]; then
-        cp -r ${MY_DIR}/config .
+        cp -r "${MY_DIR}/config" .
 fi
 
 # This is left here commented out, use it in case you have selinux
