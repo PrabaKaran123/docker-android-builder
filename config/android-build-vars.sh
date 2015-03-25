34cd6ecaffc3f77bfed03246a17c1506985e5d42
Ilkka Tengvall <ilkka.tengvall@iki.fi>
2015-03-25 06:35:49 +0200

    Added sample config for TF700T OmniRom

--- config/android-build-vars.sh
+++ config/android-build-vars.sh
@@ -1,21 +1,31 @@
 # Update the following for your flavor of Android  
 ######################################################################## 
-## Example - these parameters will build the android-5.0 (lollipop)    #
-##           branch of OmniRom for the Nexus 5                         #
-# BUILD_NAME="OmniRom"                                                 #
-# REPO="git://github.com/omnirom/android.git"                          #
-# BRANCH="android-5.0"                                                 #
-# DEVICE="hammerhead"                                                  #
-# BRLUNCH="brunch"                                                     #
-######################################################################## 
 ## Example - these parameters will build the android-5.1 (lollipop)    #
-##           Zombi version of OmniRom for the Asus TF700T              #
-# BUILD_NAME="Zombi-LP-5.1"                                            #
-# REPO="git://github.com/zombi-x-lp/platform_manifest.git"             #
-# BRANCH="lp5.1"                                                       #
-# DEVICE="zombi_tf700t-userdebug"                                      #
-# BRLUNCH="lunch"                                                      #
-# MAKE="mka bacon"                                                     #
+##           branch of OmniRom for the Nexus 5                         #
+# BUILD_NAME="OmniRom"
+# REPO="git://github.com/omnirom/android.git"
+# BRANCH="android-5.0"
+# DEVICE="hammerhead"
+# BRLUNCH="brunch"
+# MAKE=""
+######################################################################## 
+## Example - these parameters will build the android-5.1 (lollipop)
+##           branch of OmniRom for the Asus TF700T
+# BUILD_NAME="OmniRom"
+# REPO="git://github.com/omnirom/android.git"
+# BRANCH="android-5.1"
+# DEVICE="tf700t"
+# BRLUNCH="brunch"
+# MAKE=""
+######################################################################## 
+## Example - these parameters will build the android-5.1 (lollipop)
+##           Zombi version of OmniRom for the Asus TF700T
+# BUILD_NAME="Zombi-LP-5.1"
+# REPO="git://github.com/zombi-x-lp/platform_manifest.git"
+# BRANCH="lp5.1"
+# DEVICE="zombi_tf700t-userdebug"
+# BRLUNCH="lunch"
+# MAKE="mka bacon"
 ######################################################################## 
 
 BUILD_NAME="Zombi-LP-5.1"
